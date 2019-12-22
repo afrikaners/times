d7cea4f1c985eda88c735d9a2f604ee76d3faf48
T Jordaan <marthinus.jordaan@gmail.com>
2019-12-22 23:20:33 +0800

    quick test to check multiple times returned

--- TimesOld/Controllers/TimesController.cs
+++ TimesOld/Controllers/TimesController.cs
@@ -26,8 +26,7 @@ namespace Times.Controllers
         {
             var client = new RestClient("http://au-journeyplanner.silverrailtech.com");
             var awst_now = TimeZoneInfo.ConvertTime(DateTime.Now,
-                 TimeZoneInfo.FindSystemTimeZoneById("W. Australia Standard Time")).ToString("yyyy-MM-ddTHH:mm");
-            awst_now = DateTime.Now.AddHours(10).ToString("yyyy-MM-ddTHH:mm");
+                 TimeZoneInfo.FindSystemTimeZoneById("W. Australia Standard Time")).AddHours(10).ToString("yyyy-MM-ddTHH:mm");
             var request = new RestRequest($"/journeyplannerservice/v2/REST/DataSets/PerthRestricted/StopTimetable?ApiKey=40c5877a-7b0f-4045-9544-046d4f348bc2&format=json&StopUid=PerthRestricted%3A133&Time={HttpUtility.UrlEncode(awst_now)}&ReturnNotes=true&IsRealTimeChecked=false", Method.GET);
 
             IRestResponse response = client.Execute(request);

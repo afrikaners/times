5a99f56cb5309c68d68c201ca1148fea9beb1f1e
T Jordaan <marthinus.jordaan@gmail.com>
2019-12-26 07:22:54 +0800

    Fix copy paste error

--- TimesOld/ConstantValues.cs
+++ TimesOld/ConstantValues.cs
@@ -8,8 +8,8 @@ namespace TimesOld
     public static class ConstantValues
     {
         public const string ApiKey = "40c5877a-7b0f-4045-9544-046d4f348bc2";
-        public const string BaseUrl = "http://au-journeyplanner.silverrailtech.com/journeyplannerservice/v2/REST/DataSets/PerthRestricted/Trip";
-        public const string TripsTemplate = "?ApiKey={0}&format=json&StopUid=PerthRestricted%3A{1}&Time={2}&ReturnNotes=false&IsRealTimeChecked=false";
-        public const string TripDetailTemplate = "?ApiKey={0}&format=json&TripUid=PerthRestricted%3A{1}&TripDate={2}&IsMappingDataReturned=false";
+        public const string BaseUrl = "http://au-journeyplanner.silverrailtech.com";
+        public const string TripsTemplate = "/journeyplannerservice/v2/REST/DataSets/PerthRestricted/StopTimetable?ApiKey={0}&format=json&StopUid=PerthRestricted%3A{1}&Time={2}&ReturnNotes=false&IsRealTimeChecked=false";
+        public const string TripDetailTemplate = "/journeyplannerservice/v2/REST/DataSets/PerthRestricted/Trip?ApiKey={0}&format=json&TripUid=PerthRestricted%3A{1}&TripDate={2}&IsMappingDataReturned=false";
     }
 }
